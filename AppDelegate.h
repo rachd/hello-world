42ee59b134605a96656cee940a893436cf6c55df
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-02-09 20:36:43 -0500

    set up AppDelegate

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello-World
+//
+//  Created by Rachel Dorn on 2/9/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Hello-World
+//
+//  Created by Rachel Dorn on 2/9/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
